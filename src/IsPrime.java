3184b447479fb2c9eb5460f6a1e8442b67c06745
GARGI077 <singhgargi077@gmail.com>
2022-03-30 12:21:41 +0530

    DSA

---
+++ src/IsPrime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class IsPrime {
+    public static void main(String[] args) {
+        
+        Scanner in=new Scanner(System.in);
+        int num=in.nextInt();
+        boolean isPrime=true;
+        
+        for(int fact=1;fact*fact<=num;fact++)
+        {
+            if(num%fact==0) {
+                isPrime = false;
+                break;
+            }
+        }
+        
+        if(isPrime)
+        {
+            System.out.println("Is Prime ");
+        }
+        else {
+            System.out.println("Not Prime ");}
+        
+        
+    }
+}
