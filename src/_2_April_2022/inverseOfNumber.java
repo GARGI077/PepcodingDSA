9cd4266bc067c49f9e7be60b1d0bc1d593fa7f93
GARGI077 <singhgargi077@gmail.com>
2022-04-04 23:29:15 +0530

    DSA

---
+++ src/_2_April_2022/inverseOfNumber.java
@@ -0,0 +1,24 @@
+package _2_April_2022;
+
+import java.util.Scanner;
+
+public class inverseOfNumber {
+    public static void main(String[] args) {
+
+        Scanner in =new Scanner(System.in);
+
+        int n=in.nextInt();
+        int ans =0;
+        int pos=1;
+        while(n!=0)
+        {
+            int digit=n%10;
+            ans +=pos*(Math.pow(10,digit-1));
+            n=n/10;
+            pos++;
+        }
+        System.out.println(ans);
+
+        in.close();
+    }
+}
