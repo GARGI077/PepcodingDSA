a6b94cbd6513f6bb68ed0280b89bab397d80399a
GARGI077 <singhgargi077@gmail.com>
2022-04-09 23:27:39 +0530

    DSA

---
+++ src/_9_April_2022/Pattern14.java
@@ -0,0 +1,21 @@
+package _9_April_2022;
+
+import java.util.Scanner;
+
+public class Pattern14 {
+    public static void main(String[] args) {
+        // write ur code here
+        Scanner in = new Scanner(System.in);
+
+        // write ur code here
+        int n=in.nextInt();
+        int product=1;
+
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+
+
+    }
+}
