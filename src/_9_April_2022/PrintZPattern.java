b163177b0942e83f2fccda79d201d0c2d320e606
GARGI077 <singhgargi077@gmail.com>
2022-04-09 23:18:41 +0530

    DSA

---
+++ src/_9_April_2022/PrintZPattern.java
@@ -0,0 +1,10 @@
+package _9_April_2022;
+
+import java.util.Scanner;
+
+public class PrintZPattern {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+
+    }
+}
