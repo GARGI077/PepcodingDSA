c0972bce0bff088fd3cedb84f82130b0bf8b711c
GARGI077 <singhgargi077@gmail.com>
2022-04-14 23:17:00 +0530

    DSA

---
+++ src/Patterns/Pattern13.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern13 {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+
+        int num=in.nextInt();
+
+        for(int row=1;row<num;row++)
+        {
+            int ncr = 1;
+            for(int col=0;col<=row;col++)
+            {
+                System.out.print(ncr+"\t");
+                int n = row;
+                int r = col;
+                int next = ((n-r)*ncr)/(r+1);
+                ncr = next;
+
+            }
+            System.out.println();
+        }
+    }
+}
