29724269362497157257ac2f75b10a96189934b5
GARGI077 <singhgargi077@gmail.com>
2022-04-21 22:11:04 +0530

    DSA

---
+++ src/Array/BinarySearch.java
@@ -0,0 +1,48 @@
+package Array;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+
+        int[] a=new int[n];
+
+        for(int i=0;i<n;i++)
+        {
+            a[i]=in.nextInt();
+        }
+
+        int target = in.nextInt();
+        int index=search(a,target);
+    }
+    public static int search(int[] nums, int target) {
+
+        int l=0;
+        int r=nums.length-1;
+
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+
+            if(target==nums[mid])
+            {
+                return mid;
+            }
+            else
+            if(target>nums[mid])
+            {
+                l=mid+1;
+            }
+            else
+            if(target<nums[mid])
+            {
+                r=mid-1;
+            }
+
+        }
+        return -1;
+    }
+
+}
