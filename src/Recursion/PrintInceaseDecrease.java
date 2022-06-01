ffb565b81591c1cc621c92fe748c70b684d03079
GARGI077 <singhgargi077@gmail.com>
2022-06-01 23:20:37 +0530

    Recursion

---
+++ src/Recursion/PrintInceaseDecrease.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class PrintInceaseDecrease {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner in =new Scanner(System.in);
+        int num=in.nextInt();
+        pdi(num);
+
+    }
+
+    public static void pdi(int n){
+
+        if(n>0)
+        {
+            System.out.println(n);
+            pdi(n-1);
+            System.out.println(n);
+        }
+    }
+
+}
